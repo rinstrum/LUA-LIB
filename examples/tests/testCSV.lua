7171b09661781372940899635c8a66446a46afe5
merrick.heley <merrick.heley@gmail.com>
2014-03-28 16:28:41 +1000

    Made documentation changes to rinCSV file and improved the test documentation

--- examples/tests/testCSV.lua
+++ examples/tests/testCSV.lua
@@ -9,31 +9,45 @@ package.path = "/home/src/?.lua;" .. package.path
 
 local csv = require "rinLibrary.rinCSV"
 
--------------------------------------------------------------------------------
--- Configure the registers.csv file structure
-db = {}   
-csv.addTableDB(db,'materials', 
-                {['fname'] = 'materials.csv',
-                 ['labels'] = {'Mat.No','Name','Density'},
-                 ['data'] = {{1,'CEMENT',1.0}}}
-                )
-csv.addTableDB(db,'recipes', 
-                {['fname'] = 'recipe.csv',
-                 ['labels'] = {'Rec.No','Mat1','Target1','Mat2','Target2'},
-                 ['data'] = {{1,1,1000,0,0}}}
-                )
+-- Create an empty database
+local db = {}
 
+-- Create a materials table for the database. This will be called materials.csv,
+-- and will have the headings Mat.No, Name, and Density.
+-- This will also populate the database with some initial data of a material 
+-- called cement that has a Mat.No of 1 and a Density of 1.0.
+csv.addTableDB(db,'materials', {
+                ['fname'] = 'materials.csv',
+                ['labels'] = {'Mat.No','Name','Density'},
+                ['data'] = {{1,'CEMENT',1.0}}
+            })
+
+-- Create a recipes table for the database. This file will have 3 rows of 
+-- initial data.
+csv.addTableDB(db,'recipes', {
+                ['fname'] = 'recipe.csv',
+                ['labels'] = {'Rec.No','Mat1','Target1','Mat2','Target2'},
+                ['data'] = {{1,1,1000,0,0},
+                            {2,1,2000,0,0}}
+            })
+
+-- Attempt to load the database from existing files on disk. If 
 csv.loadDB(db)  
+
 print('Database Contents:')
 print(csv.tostringDB(db,10))
+
 print('Material Table Details')
 print(string.format('%d Columns, %d Rows',csv.numColsCSV(db.materials),csv.numRowsCSV(db.materials)))
+
 print('Names from Column 2')
 print(csv.tostringCol(csv.getColCSV(db.materials,2)))
+
 print("Material Data for material 'CEMENT'")
 local row, line = csv.getLineCSV(db.materials,'CEMENT',2)
 print(' Row : ', row)
 print('Line : ',csv.tostringLine(line,10))
+
 print('Change Density to 5')
 line[3] = 5
 csv.replaceLineCSV(db.materials,row,line)
@@ -42,7 +56,7 @@ print(csv.tostringCSV(db.materials,10))
 print('---------------------------------------------------')
 print('Logging Data to File')
 
-log = csv.loadCSV({['fname'] = 'test1.csv',
+local log = csv.loadCSV({['fname'] = 'test1.csv',
                    ['labels'] = {'Target','Actual','Fill Time'},
                    ['data'] = {}})
 -- addLineCSV addsline ot the ram copy but does not save to disk
