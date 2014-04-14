18e58e84f84abcaa3afa78e46f23388378d05227
Pauli <paul.dale@rinstrum.com>
2014-04-15 09:30:24 +1000

    Slight rework of the power of ten memo-function to ensure maximum run time for a value not in the table is O(log n) rather than O(n).  The table is partially populated during this computation rather than fully.

--- src/rinLibrary/K400Util.lua
+++ src/rinLibrary/K400Util.lua
@@ -17,18 +17,20 @@ local bit32 = require "bit"
 -- A table containing integral powers of ten then their reciprocals.
 --
 -- This is implemented as a memo function so as to avoid an expensive
--- exponentiation or series of repeated multiplications.
--- It would be possible to speed the computation while partially filling
--- the table by taking advantage of the binary digits of n
+-- exponentiation or repeatitive sequences of multiplications.  The maximum
+-- recursion depth is O(log k) during calculation.  We also populate some
+-- small positive integral values because they are the most likely to be
+-- requied and this saves a small amount of computation.
 local powersOfTen = 
     setmetatable({ 10, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7, [0] = 1 },
         { __index = function (t, k)
                         if k < 0 then
                             t[k] = 1 / t[-k]
+                        elseif k % 2 == 1 then
+                            t[k] = 10 * t[k-1]
                         else
-                            for i = 1+#t, k do
-                                t[i] = 10 * t[i-1]
-                            end
+                            local z = t[k/2]
+                            t[k] = z * z
                         end
                         return t[k]
                     end
