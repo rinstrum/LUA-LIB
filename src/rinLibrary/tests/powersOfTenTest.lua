f3a3368f99ef59701fc57384e14ad69dbb69e316
Pauli <paul.dale@rinstrum.com>
2014-04-15 14:24:47 +1000

    Greatly simplify the power of ten unit test.

--- src/rinLibrary/tests/powersOfTenTest.lua
+++ src/rinLibrary/tests/powersOfTenTest.lua
@@ -1,28 +1,24 @@
 local p10 = require "rinLibrary.powersOfTen"
+local n, f = 0, 0
 
 -- Load some mysterious values up
 local a = p10[111]
 a = p10[39]
 a = p10[128]
 a = p10[-122]
-for i=12, -40, -1 do
+for i= -40, 12 do
     a = p10[i]
 end
 
+
 -- Compare these values against what they sohuld be
 for k,v in pairs(p10) do
-    local s = string.format("%.0e", v)
-    local r = tostring(math.abs(k))
-    if math.abs(k) < 10 then
-        r = "0" .. r
+    local s = tonumber(string.format("%.0e", v):sub(3), 10)
+    if k ~= s then
+        print("test failed: " .. s .. " is not the expected " .. k)
+        f = f + 1
     end
-    if k < 0 then
-        r = "-" .. r
-    else
-        r = "+" .. r
-    end
-    r = "1e" .. r
-    if r ~= s then
-        print("test failed: " .. s .. " is not the expected " .. r)
-    end
+    n = n + 1
 end
+
+print("Failed " .. f .. " tests out of " .. n)
