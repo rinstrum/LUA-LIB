15440d215989a06bcb67ed74de206ac418700c45
Pauli <paul.dale@rinstrum.com>
2014-05-21 12:15:31 +1000

    Localise another global that shouldn't be.

--- src/rinLibrary/K400Analog.lua
+++ src/rinLibrary/K400Analog.lua
@@ -22,62 +22,63 @@ _M.REG_ANALOGUE_DATA = 0x0323
 _M.REG_ANALOGUE_TYPE = 0xA801
 _M.REG_ANALOGUE_CLIP = 0xA806
 _M.REG_ANALOGUE_SOURCE = 0xA805  -- must be set to option 3 "COMMS" if we are to control it via the comms
-  
+
 _M.CUR = 0
 _M.VOLT = 1
 
-local curAnalogType = -1 
+local curAnalogType = -1
+local lastAnalogue = nil
 
 _M.ANALOG_COMMS = 3
 -------------------------------------------------------------------------------
 -- Set the analog output type
--- @param src Source for output.  
+-- @param src Source for output.
 -- Must be set to ANALOG_COMMS to control directly
 function _M.setAnalogSource(src)
    _M.sendRegWait(_M.CMD_WRFINALDEC,
                 _M.REG_ANALOGUE_SOURCE,
                 src)
-  _M.saveSettings()                
+  _M.saveSettings()
 end
-                                         
+
 -------------------------------------------------------------------------------
 -- Set the analog output type
 -- @param typ Type for output (.CUR or .VOLT)
 -- @return The previous analog output type
 function _M.setAnalogType(typ)
     local prev = curAnalogType
-    
+
     if typ == _M.CUR then
         curAnalogType = _M.CUR
     else
         curAnalogType = _M.VOLT
-    end  
-    
-    if curAnalogType ~= prev then 
+    end
+
+    if curAnalogType ~= prev then
         _M.sendRegWait(_M.CMD_WRFINALDEC,
                 _M.REG_ANALOGUE_TYPE,
-                curAnalogType) 
+                curAnalogType)
     end
     return prev
-end   
+end
 
 -------------------------------------------------------------------------------
 -- Control behaviour of analog output outside of normal range.
--- If clip is active then output will be clipped to the nominal range 
+-- If clip is active then output will be clipped to the nominal range
 -- otherwise the output will drive to the limit of the hardware
 -- @function setAnalogClip
 -- @param c 0 for clipping disabled, 1 for clipping enabled
-_M.setAnalogClip = _M.preconfigureMsg(  _M.REG_ANALOGUE_CLIP, 
+_M.setAnalogClip = _M.preconfigureMsg(  _M.REG_ANALOGUE_CLIP,
                                         _M.CMD_WRFINALDEC, "noReply")
 -------------------------------------------------------------------------------
 -- Sets the analog output to minimum 0 through to maximum 50,000
 -- @param raw value in raw counts (0..50000)
 function _M.setAnalogRaw(raw)
-   if _M.lastAnalogue ~= raw then 
+   if lastAnalogue ~= raw then
        _M.sendReg(_M.CMD_WRFINALDEC, _M.REG_ANALOGUE_DATA, raw)
-       _M.lastAnalogue = raw
-   end    
-end                                         
+       lastAnalogue = raw
+   end
+end
 -------------------------------------------------------------------------------
 -- Sets the analogue output to minimum 0.0 through to maximum 1.0
 -- @param val value 0.0 to 1.0
@@ -98,7 +99,7 @@ end
 -- @param val value 0.0 to 10.0
 function _M.setAnalogVolt(val)
   _M.setAnalogType(_M.VOLT)
-  val = val / 10 
+  val = val / 10
  _M.setAnalogVal(val)
 end
 
