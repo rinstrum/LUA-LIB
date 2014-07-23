838f5f9ec529b3e78df4f1c6c8c2f0143ea7851c
Sean Liddle <sean.liddle@rinstrum.com>
2014-07-23 17:26:47 +1000

    update stream test, still needs work

--- tests/network/stream.lua
+++ tests/network/stream.lua
@@ -8,7 +8,7 @@ require "tests.assertions"
 describe("Streaming #stream", function ()
     local net = require "tests.network"
     local timers = require 'rinSystem.rinTimers.Pack'
-    local app, upper, lower
+    local app, upper, lower, death
 
     setup(function()
         app, upper, lower = net.openDevices()
@@ -29,7 +29,7 @@ describe("Streaming #stream", function ()
 
 -- Need to verify streaming works.
 -- Need to verify streaming multiple registers.
--- Need to verify differnt stream timings.
+-- Need to verify different stream timings.
     describe("timings", function()
         local s, count
 
@@ -38,12 +38,14 @@ describe("Streaming #stream", function ()
         after_each(function()
             upper.removeStream(s)
             s = nil
+            upper.setStreamFreq('onchange')
         end)
 
         local function increment() count = count + 1 end
 
         it("auto10", function()
-            s = upper.addStream('gross', increment, 'auto10')
+            upper.setStreamFreq('auto10')
+            s = upper.addStream('gross', increment, 'always')
             timers.addTimer(0, .1, function() count = 0 end)
             timers.addTimer(0, 1.1, function()
                 assert.between(count, 8, 12)
@@ -52,7 +54,8 @@ describe("Streaming #stream", function ()
         end)
 
         it("auto3", function()
-            s = upper.addStream('gross', increment, 'auto3')
+            upper.setStreamFreq('auto3')
+            s = upper.addStream('gross', increment, 'always')
             timers.addTimer(0, .1, function() count = 0 end)
             timers.addTimer(0, 2.1, function()
                 assert.between(count, 4, 8)
@@ -60,8 +63,9 @@ describe("Streaming #stream", function ()
             upper.delay(2.2)
         end)
 
-        it('auto1', function()
-            s = upper.addStream('gross', increment, 'auto1')
+        it("auto1", function()
+            upper.setStreamFreq('auto1')
+            s = upper.addStream('gross', increment, 'always')
             timers.addTimer(0, .1, function() count = 0 end)
             timers.addTimer(0, 5.1, function()
                 assert.between(count, 4, 6)
