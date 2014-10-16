bfc5e7fe3d1558c5c916b65d3ab8f0ec9aff5006
Pauli <paul.dale@rinstrum.com>
2014-10-17 09:42:08 +1000

    Two improvements to the recovery script.
    * Allow scripts named recovery.lua to execute as soon as they are discovered on a USB storage device.  This occurs without manual intervention.
    
    * Allow all .opk and .rpk packages at the root of a storage device to be installed manually.

--- src/autostart.lua
+++ src/autostart.lua
@@ -9,7 +9,7 @@
 -- @copyright 2014 Rinstrum Pty Ltd
 -------------------------------------------------------------------------------
 
-require 'posix'
+local posix = require 'posix'
 
 local threshold = 30        -- Seconds running to consider the start a failure
 local failure = 3           -- Number of sequential failures before terminating
@@ -25,21 +25,16 @@ local function time()
 end
 
 return function(directory, main)
-    local running = true
     local count = 0
 
-    while running do
+    while count <= failure do
         local s = time()
         local r = os.execute('cd ' .. directory .. ' && /usr/local/bin/lua ' .. main)
         if r == 0 then
             count = 0
         else
-            local d = time() - s
-            if d < threshold then
+            if time() - s < threshold then
                 count = count + 1
-                if count > failure then
-                    running = false
-                end
             else
                 count = 1
             end
@@ -50,12 +45,13 @@ return function(directory, main)
     local rinApp = require 'rinApp'
     local usb = require "rinLibrary.rinUSB"
     local dev = rinApp.addK400('K401')
-    local usbPath = nil
+    local usbPath, usbPackages = nil, nil
 
 -------------------------------------------------------------------------------
 -- Copy the execution directory to USB as a tar file
 -- @local
     local function copyTo()
+        dev.write('topRight', 'SAVE', 'wait, time=.3')
         local yr, mo, da = dev.RTCreadDate()
         local hr, mi, se = dev.RTCreadTime()
         local dest = string.format('%s/save-%s-%s%s%s%s%s%s.tar',
@@ -71,6 +67,7 @@ return function(directory, main)
 -- Copy lua, ini, csv, ris and txt files from the USB to the execution directory
 -- @local
     local function copyFrom()
+        dev.write('topRight', 'LOAD', 'wait, time=.3')
         for _, s in pairs{ 'lua', 'luac', 'ini', 'csv', 'ris', 'txt' } do
             os.execute('cp '..usbPath..'/*.'..s..' '..directory..'/')
         end
@@ -80,13 +77,30 @@ return function(directory, main)
     end
 
 -------------------------------------------------------------------------------
+-- Install all available packages from the USB
+-- @local
+    local function installPackages()
+        dev.write('topRight', 'PKGS', 'wait, time=.3')
+        for _, pkg in pairs(usbPackages) do
+            os.execute('/usr/local/bin/rinfwupgrade ' .. pkg)
+        end
+        -- Package installation kills the Lua infrastructure so reboot now
+        os.execute('reboot')
+    end
+
+-------------------------------------------------------------------------------
 -- Change the mutable display fields
 -- @local
     local function updateDisplay()
-        local f2, f3
+        local f2, f3, f4
         if usbPath ~= nil then
+            local prompt = 'F1 EXIT F2 USB> F3 >USB'
+            if usbPackages then
+                prompt = prompt .. ' OK PKGS'
+                f4 = installPackages
+            end
             dev.write('bottomLeft', 'READY')
-            dev.write('bottomRight', 'F1 EXIT F2 USB> F3 >USB', 'align=right')
+            dev.write('bottomRight', prompt, 'align=right')
             f2, f3 = copyFrom, copyTo
         else
             dev.write('bottomLeft', 'WAIT USB')
@@ -94,6 +108,7 @@ return function(directory, main)
         end
         dev.setKeyCallback('f2', f2, 'short')
         dev.setKeyCallback('f3', f3, 'short')
+        dev.setKeyCallback('ok', f4, 'short')
     end
 
     dev.clearAnnunciators('all')
@@ -104,11 +119,21 @@ return function(directory, main)
 
     usb.setStorageAddedCallback(function(where)
         usbPath = where
+        usbPackages = posix.glob(where .. '/*.[oOrR][Pp][kK]')
+        local recover = posix.glob(where .. '/[rR][eE][cC][oO][vV][eE][rR][yY].[lL][uU][aA]')
+        if recover then
+            dev.write('bottomRight', #recover > 1 and 'SCRIPTS' or 'SCRIPT')
+            dev.write('bottomLeft', 'RUNNING', 'wait, time=.3, align=right')
+            for _, s in pairs(recover) do
+                os.execute('/usr/local/bin/lua ' .. s)
+            end
+        end
         updateDisplay()
     end)
 
     usb.setStorageRemovedCallback(function()
         usbPath = nil
+        usbPackages = nil
         updateDisplay()
     end)
 
