101aaa5d93778de89ef23a87857828e7f2fbf4ca
Pauli <paul.dale@rinstrum.com>
2014-11-04 08:42:22 +1000

    Add the sandbox application to the Lua examples.
    This application lets you run Lua commands from the command line.

---
+++ examples/sandbox.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env lua
+-------------------------------------------------------------------------------
+-- Interactive snadbox for experimenting with Lua and our library
+--
+-- Direct command line access to library functions
+--
+-------------------------------------------------------------------------------
+
+--=============================================================================
+-- Some of the more commonly used modules as globals
+rinApp = require 'rinApp'               -- load in the application framework
+timers = require 'rinSystem.rinTimers'  -- load in some system timers
+dbg    = require 'rinLibrary.rinDebug'  -- load in a debugger
+
+--=============================================================================
+-- Connect to the instrument you want to control
+dwi = rinApp.addK400("K401")            -- local K401 instrument
+
+function callback(name)
+    local msg = 'call back ' .. name .. ':'
+    return function(...) print(msg, ...) end
+end
+
+--=============================================================================
+-- Main Application
+--=============================================================================
+rinApp.setUserTerminal(function(s)
+    local ok, err = pcall(loadstring(s))
+    if not ok then
+        print(err)
+	    return false
+    end	 
+    return true
+end)
+
+rinApp.run()
