096290fef7a0f06df8230e11ddba2655cd875a73
Pauli <paul.dale@rinstrum.com>
2015-01-19 13:39:56 +1000

    Better manage the recipe names and the pick list. Create a new recipe if an attempt is made to edit when there are none.

--- examples/recipe.lua
+++ examples/recipe.lua
@@ -21,12 +21,10 @@ local recipeMenu, managementMenu
 -- Our recipe has its own menu for creation and editing.  This defined the
 -- fields and registers in the recipe.
 recipeMenu = device.createMenu { 'RECIPES' }
-        .menu { 'TIMERS' }
             .string   { 'recipe', '', prompt='NAME' }
             .register { 'fill_delay', 'stage_pulse_time1', prompt='FIL.DLY' }
             .register { 'chk_delay', 'stage_fill_jog_set2', prompt='DMP.TIM' }
             .integer  { 'clamp_release', 0, prompt='BAG.DLY' }
-            .fin()
 
 
 -- Load the recipe database
@@ -36,6 +34,15 @@ local recipeDatabase = csv.loadCSV{
     labels = menuColumns
 }
 local recipeCol = csv.labelCol(recipeDatabase, 'RECIPE')
+local currentRecipe = 1
+
+-- Get the name of the nth recipe or null if the index isn't valid
+local function getRecipeName(n)
+    if n > 0 and n <= csv.numRowsCSV(recipeDatabase) then
+        return recipeDatabase.data[n][recipeCol]
+    end
+    return ''
+end
 
 -- Utility function to commit changes to flash
 local function commit()
@@ -53,7 +60,8 @@ local function pickByName()
     local r = managementMenu.getValue'RECIPE'
     local rec = csv.getRecordCSV(recipeDatabase, r, 'RECIPE')
     if rec ~= nil then
-        managementMenu.setValues(rec)
+        recipeMenu.setValues(rec)
+        currentRecipe = res
     end
 end
 
@@ -64,6 +72,8 @@ local function deleteRecipe()
     if n ~= nil then
         csv.remLineCSV(recipeDatabase, n)
         commit()
+        currentRecipe = n > 1 and (n-1) or 1
+        managementMenu.setValue('RECIPE', getRecipeName(currentRecipe))
     end
 end
 
@@ -74,6 +84,7 @@ local function addRecipe()
 
     for _, v in ipairs(menuColumns) do
         row[csv.labelCol(recipeDatabase, v)] = val[v]
+        print('row', v, val[v])
     end
 
     local ids = csv.getColCSV(recipeDatabase, 'RECIPE')
@@ -81,16 +92,22 @@ local function addRecipe()
     -- The search for a missing slot can be done a lot more efficiently
     for id = 1, csv.numRowsCSV(recipeDatabase) + 1 do
         local new, again = 'R'..id, false
-        for i = 1, #ids do
-            if ids[i] == new then
-                again = true
-                break
+        if ids then
+            for i = 1,  #ids do
+                if ids[i] == new then
+                    again = true
+                    break
+                end
             end
         end
         if not again then
+            print('new', new)
             row[recipeCol] = new
             csv.addLineCSV(recipeDatabase, row)
+            currentRecipe = csv.numRowsCSV(recipeDatabase)
             commit()
+            populateRecipes()
+            managementMenu.setValue('RECIPE', new)
             return
         end
     end
@@ -104,12 +121,27 @@ managementMenu = device.createMenu{ 'RECIPES' }
             .item { 'QUIT', exit=true }
 
 -- Provide a default recipe (the first one if any are present)
-if csv.numRowsCSV(recipeDatabase) > 0 then
-    managementMenu.setValue('RECIPE', recipeDatabase.data[1][recipeCol])
+managementMenu.setValue('RECIPE', getRecipeName(currentRecipe))
+
+-- local function edit recipe
+local function editRecipe()
+    if csv.numRowsCSV(recipeDatabase) == 0 then
+        addRecipe()
+    end
+    recipeMenu.run()
+    local val = recipeMenu.getValues()
+    local row = {}
+    for i = 1, #menuColumns do
+        table.insert(row, val[menuColumns[i]])
+    end
+    csv.replaceLineCSV(recipeDatabase, currentRecipe, row)
+    commit()
+    populateRecipes()
+    managementMenu.setValue('RECIPE', getRecipeName(currentRecipe))
 end
 
 -- Hook up the user interface menus to the keys
-device.setKeyCallback('f1', function() recipeMenu.run() commit() end, 'short')
+device.setKeyCallback('f1', editRecipe, 'short')
 device.setKeyCallback('f2', managementMenu.run, 'short')
 
 -- Produce some welcome text
